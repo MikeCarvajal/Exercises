5438b2840a030dff6bffc7df5654fbbbcfc3ba13
MikeCarvajal <michael_carvajalm@outlook.com>
2025-01-11 17:39:37 -0500

    Exercise with JS loops

---
+++ Java/exer1.js
@@ -0,0 +1,76 @@
+const character = "#";
+const count = 8;
+const rows = [];
+let inverted = true;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+// TODO: use a different type of loop
+
+for (let i = 1; i <= count; i++) {
+    if (inverted) {
+      rows.unshift(padRow(i, count));
+    } else {
+      rows.push(padRow(i, count));
+    }
+  }
+/*for (let i = 1; i <= count; i++) {
+  rows.push(padRow(i, count));
+}*/
+
+/*while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}*/
+
+/*for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
+}*/
+let result = ""
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
+
+for (let i = 1; i <= count; i++) {
+    rows.push(padRow(i, count));
+    rows.unshift(padRow(i,count));
+  }
+
+  for (let i = 1; i <= count; i++) {
+    if (inverted) {
+      rows.unshift(padRow(i, count));
+    } else {
+      rows.push(padRow(i, count));
+    }
+  }
+
+//Pyramide with loop
+
+const character = "#";
+const count = 20;
+const rows = [];
+let inverted = true;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
---
+++ Java/exer2.js
@@ -0,0 +1,49 @@
+//function para sacar la suma y total / para el numero de array
+
+function getAverage(scores) {
+    let total = 0;
+    for(const score of scores) {
+      total = total + score;
+    }
+    const average = total/scores.length;
+    return average;
+  }
+   
+  
+  
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+/*Now the teacher needs your help converting the student score to a letter grade.
+
+Complete the getGrade function that takes a number score as a parameter. Your function should return a string representing a letter grade based on the score.
+
+Here are the scores and their corresponding letter grades:
+
+Score Range	Grade
+100	"A++"
+90 - 99	"A"
+80 - 89	"B"
+70 - 79	"C"
+60 - 69	"D"
+0 - 59	"F"
+Tips
+
+Remember that you learned about conditional statements (if, else if, and else).
+Remember that you learned about comparison operators (>, <, >=, <=, ===).*/
+
+function getGrade(score) {
+    if (score === 100) {
+      return "A++";
+    } else if (score >= 90) {
+      return "A";
+    } else if (score >= 80) {
+      return "B";
+    } else if (score >= 70) {
+      return "C";
+    } else if (score >= 60) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
