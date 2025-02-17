b95b2f95724ac030ed42f6fa1728eefa82cc3ff5
MikeCarvajal <michael_carvajalm@outlook.com>
2025-02-17 16:52:53 -0500

    New program game sisor, rock, paper

---
+++ Java/exerc6/exerc6.js
@@ -0,0 +1,16 @@
+function getRandomComputerResult (){
+    const options = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random()*options.length);
+    return options[randomIndex];
+}
+console.log(getRandomComputerResult());
+
+
+function hasPlayerWonTheRound(player, computer) {
+    const player = options.length
+    const computer = 
+    if player == "rock"
+}
+
+console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
